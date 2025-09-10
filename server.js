c8edb590225600a3c5fede2d2dfba5c2f2c4b3c7
Mohan Raj <mohanraj31521@gmail.com>
2025-09-10 14:21:01 +0530

    changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello from Azure DevOps Project 🚀");
 });
-
+//PORT 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
