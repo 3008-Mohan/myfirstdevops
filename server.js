6dca528e44da6c4f171d56831733d3a6dea931bd
Mohan Raj <mohanraj31521@gmail.com>
2025-09-10 16:53:28 +0530

    added routes

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello from Azure DevOps Project 🚀");
 });
-//PORT 3000
+
+app.get("/about", (req, res) => {
+  res.send("This is the About Page 💡 running on Azure!");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact us at support@myfirstdevops.com 📧");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
